4eee823b3f9e64363beb27b9bba1af7a47624670
QA08 <QA08@gracetech.local>
2026-09-07 18:05:52 +0500

    Add/update test

--- cypress/e2e/2-advanced-examples/actions.cy.js
+++ cypress/e2e/2-advanced-examples/actions.cy.js
@@ -2,7 +2,7 @@
 
 context('Actions', () => {
   beforeEach(() => {
-    cy.visit('')
+    cy.visit('https://example.cypress.io/commands/actions')
   })
 
   // https://on.cypress.io/interacting-with-elements
